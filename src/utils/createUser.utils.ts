ad6818ee78e5f5696b91fe66a3bdbe8c7e66f417
Maksym Zakshevskyi <korobokvk@gmail.com>
2020-03-06 01:17:07 +0200

    check git config on this repo

--- src/utils/createUser.utils.ts
+++ src/utils/createUser.utils.ts
@@ -1,4 +1,4 @@
-export const createUser = (client, data) => {
+export const createUser = (client, data): Promise<any> => {
   return new Promise((resolve, reject) => {
     client.createUser(data, (err, response) => {
       if (err) {
