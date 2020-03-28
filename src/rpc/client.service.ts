c2854b07dc057ac62a52d19f1f14dbbdb5307812
Maksym Zakshevskyi <korobokvk@gmail.com>
2020-03-28 21:10:42 +0200

    test

--- src/rpc/client.service.ts
+++ src/rpc/client.service.ts
@@ -2,5 +2,6 @@ import { app } from '../utils/grpc.util'
 import grpc from 'grpc'
 
 const dbProviderUrl = process.env.DB_SERVICE
+console.log(dbProviderUrl)
 
 export const client = new app['DataBaseProvider'](dbProviderUrl, grpc.credentials.createInsecure())
